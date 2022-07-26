ac97ad4e23057a1765fb5619e3603befc7a685ce
ificiana <ificiana@gmail.com>
2022-07-26 17:53:48 +0530

    add typing overload

--- client/entities/spritesheet.py
+++ client/entities/spritesheet.py
@@ -1,3 +1,4 @@
+from typing import overload
 from os import PathLike
 
 import pygame
@@ -9,9 +10,13 @@ class SpriteSheet:
     def __init__(self, path: PathLike) -> None:
         self.sheet = pygame.image.load(path)
 
+    @overload
     def image_at(
-        self, rect: tuple[float, float, float, float] | pygame.Rect
+        self, rect: tuple[int, int, tuple[int, int]] | pygame.Rect
     ) -> pygame.Surface:
+        ...
+
+    def image_at(self, rect: tuple[int, int, int, int] | pygame.Rect) -> pygame.Surface:
         rect = pygame.Rect(rect)
         image = pygame.Surface(rect.size, pygame.SRCALPHA)
         image.blit(self.sheet, (0, 0), rect)
