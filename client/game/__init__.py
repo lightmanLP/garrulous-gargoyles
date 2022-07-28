aead97e4d66094780bdc0895cff0e90a3e529b37
lightmanLP <liteman1000@gmail.com>
2022-07-29 02:03:51 +0700

    remove masks

--- client/game/__init__.py
+++ client/game/__init__.py
@@ -12,17 +12,8 @@ __all__ = ("Game", )
 @event_manager.on("draw")
 def draw(game: Game):
     """Draw the sprites onto the game window"""
-    # toggle masks
-    mask = True
     game.screen.fill(struct.Color.EMERALD)
     game.sprites.draw(game.screen)
-    if mask:
-        for sprite in game.sprites:
-            sprite: Object
-            # print(sprite.mask)
-            olist = sprite.mask.outline()
-            # print(olist)
-            pygame.draw.polygon(game.screen, struct.Color.BLACK, olist, 0)
     font = pygame.font.SysFont("None", 40)
     text = font.render("Hello World!", True, struct.Color.GREEN.rgba)
     game.screen.blit(text, (300, 300))
