b986f2d1e1dcc7734cac99e0fb97f424a469d410
LrnzDC3696 <lrnzdc3696@outlook.com>
2022-07-28 14:18:15 +0800

    temporary push

---
+++ client.py
@@ -0,0 +1,24 @@
+import websockets
+import asyncio
+
+
+class Client:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+
+    def start(self):
+        asyncio.run(self.listen())
+
+    async def listen(self):
+        url = f"ws://{self.address}:{self.port}"
+
+        async with websockets.connect(url) as ws:
+            await ws.send("Hello World!")
+            while True:
+                msg = await ws.recv()
+                print(msg)
+
+
+client = Client("localhost", 8765)
+client.start()
--- server/__main__.py
+++ server/__main__.py
@@ -1,33 +1,6 @@
-from . import serve
-from . import event
+from .serve import Server
+from .event import EventHandler
 
-Server = serve.Server
-EventHandler = event.EventHandler
-
-import websockets
-import asyncio
-
-
-class Client:
-    def __init__(self, address, port):
-        self.address = address
-        self.port = port
-
-    def start(self):
-        asyncio.run(self.listen())
-
-    async def listen(self):
-        url = f"ws://{self.address}:{self.port}"
-
-        async with websockets.connect(url) as ws:
-            await ws.send({"type": "enter"})
-            while True:
-                msg = await ws.recv()
-                print(msg)
-
-
-client = Client("localhost", 8765)
-client.start()
 
 if __name__ == "__main__":
     server = Server("localhost", 8765, EventHandler())
